0946c83d4cb2ef9ecd101b38d019f80caf73d766
kevinderitis <kevin.deritis@hotmail.com>
2023-04-19 21:19:30 -0300

    nodemailer example

---
+++ mail.js
@@ -0,0 +1,37 @@
+import nodemailer from 'nodemailer';
+
+const gmailConfig = {
+    service: 'gmail',
+    auth: {
+      user: "tumail",
+      pass: "tupass"
+    }
+  }
+
+const mailContent = {
+    from: 'desde donde',
+    to: 'a quien envio',
+    subject: 'Mail con html',
+    html: `
+    <div>
+    <h1>
+        !Esto es un test con imagenes!
+    </h1>
+    <img src="cid:perrito1">
+    </div>`,
+    attachments: [{
+        filename: 'perrito1.jpg',
+        path: 'perrito1.jpg',
+        cid: 'perrito1'
+    }]
+    
+}
+
+export const sendMail = async mailConfig => {
+ const transporter = nodemailer.createTransport(gmailConfig);
+ let response = await transporter.sendMail(mailConfig);
+ console.log(response)
+}
+
+sendMail(gmailConfig, mailContent)
+
